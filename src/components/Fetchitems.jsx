dbd7bf74cc601dcf5e4adef3920378db7afbca81
MuskanSharmaa21 <mmuskansharma2110@gmail.com>
2025-05-05 22:11:51 +0530

    init

--- src/components/Fetchitems.jsx
+++ src/components/Fetchitems.jsx
@@ -15,7 +15,7 @@ const FetchItems = () => {
     const signal = controller.signal;
 
     dispatch(fetchStatusActions.markFetchingStarted());
-    fetch("http://localhost:8080/items", { signal })
+    fetch("https://myntra-backend-5za3.onrender.com/items", { signal })
       .then((res) => res.json())
       .then(({items}) => {
         dispatch(fetchStatusActions.markFetchDone());
